0b387f8ac08605d288766283c8622c3ebbb16f5a
crm2704 <carlos.rubiom@alumnos.upm.es>
2024-03-30 17:18:17 +0100

    act

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crm2704 <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 20:15:07 by crm2704           #+#    #+#             */
+/*   Updated: 2024/03/24 22:12:19 by crm2704          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	i = 0;
+	d = dest;
+	s = src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
+/*
+int	main(int argc, char const *argv[])
+{
+	char	*src;
+	char	dest[] = "adios";
+	char	*s1;
+	char	*s2;
+
+	src = "hola";
+	s1 = ft_memcpy(((void*)0), ((void*)0), 6);
+	s2 = memcpy(((void*)0), ((void*)0), 6);
+	printf("%s\n", s1);
+	printf("%s\n", s2);
+	return (0);
+}
+*/
--- libft/libft.h
+++ libft/libft.h
@@ -34,5 +34,8 @@ int		ft_strncmp(char *s1, char *s2, unsigned int n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(char *s);
+void	*ft_memcpy(void *dest, void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif

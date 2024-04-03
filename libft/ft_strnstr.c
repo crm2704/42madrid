1610910094626aed31e9df675f0664fc481050d6
crm2704 <carlos.rubiom@alumnos.upm.es>
2024-04-03 22:53:52 +0200

    act

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -11,3 +11,41 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strnstr(char *big, char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (little[0] == '\0')
+		return (big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (little[j] && little[j] == big[i])
+		{
+			j++;
+			i++;
+		}
+		if (j == ft_strlen(little))
+			return (&big[i - j]);
+        if (j > 0)
+            i--;
+        i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char const *argv[])
+{
+	char	*big;
+	char	*little;
+	char	*res;
+
+	big = "holaquetal";
+	little = "i";
+	res = ft_strnstr(big, little, 10);
+	printf("%s\n", res);
+	return (0);
+}

4f6a049e32ade89d9aa6456c23344bda0d8f7f96
Carlos Rubio <crubio@c1r6s5.42madrid.com>
2024-06-14 12:39:40 +0200

    finished todo

--- 1st_circle/ft_printf/ft_printf.c
+++ 1st_circle/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: crubio <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 19:51:26 by crubio            #+#    #+#             */
-/*   Updated: 2024/04/14 19:51:27 by crubio           ###   ########.fr       */
+/*   Updated: 2024/06/14 12:11:03 by crubio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,14 @@ int	ft_printf(char const *format, ...)
 	while (format[i])
 	{
 		if (format[i] != '%')
-		{
-			write(1, &format[i], 1);
-			ans++;
-		}
+			ans += write(1, &format[i], 1);
 		else
 		{
 			i++;
 			if (format[i])
 				ans += ft_choose_option(format[i], args);
+			else
+				ans = -1;
 		}
 		if (format[i])
 			i++;
@@ -65,6 +64,7 @@ int	ft_printf(char const *format, ...)
 	return (ans);
 }
 /*
+}
 int	main(void)
 {
 	char			*str;
@@ -77,42 +77,45 @@ int	main(void)
 	unum = -42;
 	ptr = &num;
 	int ret1, ret2;
-	printf("Testing string: ");
-	ret1 = printf("%s\n%", str);
-	ret2 = ft_printf("%s\n%", str);
+	printf("Testing string: \n");
+	ret1 = printf("%s\n", str);
+	ret2 = ft_printf("%s\n", str);
 	printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
-	printf("Testing integer: ");
+	printf("Testing integer: \n");
 	ret1 = printf("%d\n", num);
 	ret2 = ft_printf("%d\n", num);
 	printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
-	printf("Testing unsigned integer: ");
+	printf("Testing unsigned integer: \n");
 	ret1 = printf("%u\n", unum);
 	ret2 = ft_printf("%u\n", unum);
 	printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
-	printf("Testing hexadecimal: ");
+	printf("Testing hexadecimal: \n");
 	ret1 = printf("%x\n", num);
 	ret2 = ft_printf("%x\n", num);
 	printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
-	printf("Testing uppercase hexadecimal: ");
+	printf("Testing uppercase hexadecimal: \n");
 	ret1 = printf("%X\n", num);
 	ret2 = ft_printf("%X\n", num);
 	printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
-	printf("Testing pointer: ");
+	printf("Testing pointer: \n");
 	ret1 = printf("%p\n", ptr);
 	ret2 = ft_printf("%p\n", ptr);
 	printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
-	printf("Testing percentage: ");
+	printf("Testing percentage: \n");
 	ret1 = printf("%%\n");
 	ret2 = ft_printf("%%\n");
 	printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
+	printf("Testing NULL pointer: \n");
+	ret1 = printf("%p\n", NULL);
+	ret2 = ft_printf("%p\n", NULL);
+	printf("printf returned : %d, ft_printf returned: %d\n", ret1, ret2);
+	printf("Testing percentage error: \n");
+	ret1 = printf("%s\n%", str);
+	ret2 = ft_printf("%s\n%", str);
+	printf("printf returned : %d, ft_printf returned: %d\n", ret1, ret2);
+	printf("Testing mismatched format specifier and argument: \n");
+    ret1 = printf("%s\n", 12345); // This is undefined behavior
+    ret2 = ft_printf("%s\n", 12345); // This should also be undefined behavior
+    printf("printf returned: %d, ft_printf returned: %d\n", ret1, ret2);
 	return (0);
-}
-
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-	ft_printf("%p\n",  NULL);
-	printf("%p\n", NULL);
-	return 0;
-}
 */
